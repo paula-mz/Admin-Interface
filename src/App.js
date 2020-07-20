daf08b5db2b09f6527945c75a969bc01c63b5135
Paula Marsal <paula.mz_376@live.com>
2020-07-20 18:44:45 +0200

    Preparing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource,ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+//connect the data provider to the REST endpoint
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin dataProvider={dataProvider}>
+      <Resource name='users' list={ListGuesser} />
+    </Admin>
   );
 }
 
